631d40cd0a9705590a869b914eb7632dca72b672
SaniaRupa <srecommerce1561@gmail.com>
2025-08-13 00:58:34 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("CodeCareBD")
+console.log("Document")
+document.write("SR E-Commerce")
+console.log(document.getElementById("h2tag"))
+console.log(document.querySelector('#h2tag'))
+console.log(document.querySelector('h2'))
+console.log(document.querySelector('h3').innerHTML="CodeCareBD")
+
+console.log(document.querySelector('h4').innerText="<p>CodeCareBD</p>")
+
+alert('Start Coding')
